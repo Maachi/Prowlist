c75afcded6aa85d27e28a07560eaeb5dc9e6b489
Sebastian Romero <bipsa81@gmail.com>
2015-10-13 19:05:02 -0500

    Sensor admin

--- prowlist/sensors/admin.py
+++ prowlist/sensors/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from .models import *
 
-admin.site.register(Sensor)
+#admin.site.register(Sensor)
 admin.site.register(SensorLogs)
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+	list_display = ('name', 'uuid', 'serial', 'active')
+	search_fields = ['serial', 'name']
