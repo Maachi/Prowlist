c78e405a33dc13c83a80c9b8647c3839983faecd
Sebastian Romero <bipsa81@gmail.com>
2016-10-11 23:19:04 -0500

    Stage config

---
+++ prowlist/settings/stage.py
@@ -0,0 +1,11 @@
+from defaults import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../prowlist.sqlite3'),
+    }
+}
